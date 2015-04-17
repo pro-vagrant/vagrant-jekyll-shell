ee3b0c28d5a6d119546b1aa178d1efa12f56a663
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-04-17 13:03:45 +0200

    Use ruby1.9.1-dev instead of RVM

--- script.sh
+++ script.sh
@@ -4,6 +4,6 @@ echo "Installing: nodejs, lynx, rvm, ruby and jekyll..."
 apt-get update -y >>/tmp/provision-script.log 2>&1
 apt-get install nodejs -y >>/tmp/provision-script.log 2>&1
 apt-get install lynx-cur -y >>/tmp/provision-script.log 2>&1
-gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3 >>/tmp/provision-script.log 2>&1
-curl -sSL https://get.rvm.io | bash -s stable --ruby >>/tmp/provision-script.log 2>&1
-/bin/bash --login -c "gem install jekyll" >>/tmp/provision-script.log 2>&1
+apt-get install ruby1.9.1-dev -y >>/tmp/provision-script.log 2>&1
+gem install jekyll >>/tmp/provision-script.log 2>&1
+
